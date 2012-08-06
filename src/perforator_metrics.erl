d926d8279c90a0ba936da12db5cf4e55270e4530
Ignas Vyšniauskas <baliulia@gmail.com>
2012-08-06 20:31:53 +0200

    Tidy up perforator_metrics module.
    * make returned units more sane and document them
    * handle corner cases (for example when no data was gathered)
    * don't link to collectors (because we don't handle crashes currently anyway)
    * add docs and notes in TODO

--- src/perforator_metrics.erl
+++ src/perforator_metrics.erl
@@ -17,12 +17,13 @@
 init_collect() ->
     lists:map(
         fun(Collector) ->
-            spawn_link(?MODULE, Collector, [])
+            spawn(?MODULE, Collector, [])
         end,
         ?COLLECTORS
     ).
 
 retrieve(Pids) when is_list(Pids) ->
+    %% @todo: this should be performed in parallel. In general, refactoring awaits.
     {ok, lists:map(fun retrieve/1, Pids)};
 
 retrieve(Pid) ->
@@ -39,21 +40,34 @@ retrieve(Pid) ->
 %% ============================================================================
 % @todo Create a behaviour and move these to separate modules
 
+%% @doc Returns average CPU utilization ([should be] the same as returned by the UNIX
+%% `top` command)
 collect_cpu_util() ->
-    collect_cpu_util([cpu_sup:util()], ?COLLECT_INTERVAL).
+    % first util call has no meaning, so we discard it.
+    cpu_sup:util(),
+    collect_cpu_util([], ?COLLECT_INTERVAL).
 
 collect_cpu_util(Stats, SleepTime) ->
     receive {retrieve, Sender} ->
-        Sender ! {cpu_util, lists:sum(Stats) / length(Stats)}
+        % Divide by 256 to match what people are used to seeing in `top` output.
+        % http://www.erlang.org/doc/man/cpu_sup.html
+        Result = case (Stats == []) or (cpu_sup:util() == 0) of
+            true -> % cpu_sup returns if 0 if result is unavailable
+                'NA';
+            false ->
+                (lists:sum(Stats) / 256) / length(Stats)
+        end,
+        Sender ! {cpu_util, Result}
     after SleepTime ->
         collect_cpu_util([cpu_sup:util()|Stats], SleepTime)
     end.
 
+%% @doc Returns total memory used by the Erlang VM in KiloBytes.
 collect_vm_memory() ->
     collect_vm_memory([erlang:memory(total)], ?COLLECT_INTERVAL).
 collect_vm_memory(Stats, SleepTime) ->
     receive {retrieve, Sender} ->
-        Sender ! {vm_memory, lists:sum(Stats) / length(Stats)}
+        Sender ! {vm_memory, (lists:sum(Stats) / 1024) / length(Stats)}
     after SleepTime ->
         collect_vm_memory([erlang:memory(total)|Stats], SleepTime)
     end.
